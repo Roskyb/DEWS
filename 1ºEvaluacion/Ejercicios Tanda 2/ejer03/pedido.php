5caa720f533998fd1891849d30ce19063db81b20
Jorge Blanco <jorge.blanco.gil.00@gmail.com>
2021-09-26 21:38:45 +0200

    TANDA 2 EJER 3 WIP

--- 1ºEvaluacion/Ejercicios Tanda 2/ejer03/pedido.php
+++ 1ºEvaluacion/Ejercicios Tanda 2/ejer03/pedido.php
@@ -1,16 +1,25 @@
 <?php
-
+include 'utils.php';
 session_start();
-guardarArticulo();
-if(isset($_POST['nuevoArticulo'])){
 
-    if(isset($_POST['nombreArticulo']) && isset($_POST['precioArticulo'])){
+// CREAMOS VARIABLES DE ERROR SI EL SERVIDOR ESTA RECIVIENDO UNA PETICION POST
 
-        $nombre = $_POST['nombreArticulo'];
-        $precio = $_POST['precioArticulo'];
-        aniadirArticulo($nombre, $precio);
+$errorNombre = $errorPrecio = $errorDescripcion = "";
+$nombreArticulo = $precioArticulo = "";
+
+// CHECKEAMOS EL FORMULARIO
+if (isset($_POST['nuevoArticulo'])) {
+    if (empty($_POST['nombreArticulo'])) $errorNombre = "Campo nombre obligatorio";
+    else $nombreArticulo = $_POST['nombreArticulo'];
+    if (empty($_POST['precioArticulo'])) $errorPrecio = "Campo precio obligatorio";
+    else $precioArticulo = $_POST['precioArticulo'];
+
+    // SI LOS CAMPOS ESTAN LLENOS
+    if (!empty($nombreArticulo) && !empty($precioArticulo)) {
+        aniadirArticulo($nombreArticulo, $precioArticulo);
     }
 }
+
 
 function fileToArray($filePath)
 {
@@ -51,31 +60,34 @@ function dibujarArticulos()
 
 
 
-function guardarArticulo()
-{
-    if (isset($_SESSION['carrito'])) {
-        if (isset($_GET['nombre']) && isset($_GET['precio'])) {
-            $_SESSION['carrito'][] = [$_GET['nombre'], $_GET['precio']];
-        }
-    } else $_SESSION['carrito'] = [];
-}
+// function guardarArticulo()
+// {
+//     if (isset($_SESSION['carrito'])) {
+//         if (
+//             isset($_GET['nombre']) && !empty($_GET['nombre'])
+//             && isset($_GET['precio']) && !empty($_GET['precio'])
+//         ) {
+//             $_SESSION['carrito'][] = [$_GET['nombre'], $_GET['precio']];
+//         }else {
+
+//         }
+
+//     } else $_SESSION['carrito'] = [];
+// }
 
 function aniadirArticulo($nombre, $precio)
 {
-
     $articulo = $nombre . ";" . $precio;
-
     $filePath = './articulos.txt';
     $file = fopen($filePath, "a");
     fwrite($file, "\n");
     fwrite($file, $articulo);
     fclose($file);
-
 }
 
 function calcularPrecio()
 {
-    if (count($_SESSION['carrito']) == 0) echo 0 . '€';
+    if (!isset($_SESSION['carrito'])) echo 0 . '€';
     else {
         $precio = 0;
         foreach ($_SESSION['carrito'] as $art) {
@@ -123,27 +135,40 @@ function calcularPrecio()
             </tfoot>
         </table>
         <div class="container">
-        <div class="row">
-            <div class="col-6">
-                <h3>Añadir articulo</h3>
-                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
-                    <div class="mb-3">
-                        <label for="nombreArticulo" class="form-label">Articulo</label>
-                        <input type="text" class="form-control" id="nombreArticulo"  name="nombreArticulo" aria-describedby="emailHelp">
-                    </div>
-                    <div class="mb-3">
-                        <label for="precioArticulo" class="form-label">Precio</label>
-                        <input type="number" step=".01" class="form-control" id="precioArticulo" name="precioArticulo">
-                    </div>
-                    <input type="submit" class="btn btn-primary" name="nuevoArticulo" value="Enviar">
-                </form>
+            <div class="row justify-content-center">
+                <div class="col-6">
+                    <h3>Añadir articulo</h3>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" enctype="multipart/form-data">
+                        <div class="mb-3">
+                            <label for="nombreArticulo" class="form-label">Articulo</label>
+                            <input type="text" class="form-control" id="nombreArticulo" name="nombreArticulo" value="<?php echo $nombreArticulo ?>">
+                            <?php
+                            errorMessage($errorNombre);
+                            ?>
+                        </div>
+                        <div class="mb-3">
+                            <label for="precioArticulo" class="form-label">Precio</label>
+                            <input type="number" step=".01" class="form-control" id="precioArticulo" name="precioArticulo" value="<?php echo $precioArticulo ?>">
+                            <?php
+                            errorMessage($errorPrecio);
+                            ?>
+
+                        </div>
+                        <div class="mb-3">
+                            <label for="descripcionArticulo" class="form-label">Precio</label>
+                            <input type="file" class="form-control" id="descripcionArticulo" name="descripcionArticulo">
+                        </div>
+
+                        <input type="submit" class="btn btn-primary" name="nuevoArticulo" value="Enviar">
+                    </form>
+                </div>
             </div>
-        </div>
         </div>
 
 
     </main>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 
 </body>
---
+++ 1ºEvaluacion/Ejercicios Tanda 2/ejer03/utils.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+function alerta($msg)
+{
+    echo <<<HTML
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+    $msg
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+
+    </div>
+    HTML;
+}
+
+function errorMessage($msg)
+{
+    echo <<<HTML
+    <div class="text-danger">
+        $msg
+    </div>
+    HTML;
+}
