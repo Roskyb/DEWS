a2701bac514b67551746aeb9cc6689e66f24cfb0
Jorge Blanco <jorge.blanco.gil.00@gmail.com>
2021-09-27 11:25:26 +0200

    El ejercicio perdido 2

---
+++ 1ºEvaluacion/Ejercicios Tanda 2/ejer02/select_cantidad.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Seleccionar Cantidad IMG</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+	<?php
+
+	function cuantasImg()
+	{
+		$cont = 0;
+		$arrext = array("jpg", "png", "JPG", "PNG");
+		$dir = opendir("./img");
+		while ($elemento = readdir($dir)) {
+			if (!is_dir("./img" . $elemento)) {
+				$ext = explode(".", $elemento);
+				if (in_array($ext[1], $arrext)) {
+					$cont++;
+				}
+			}
+		}
+		return $cont;
+	}
+	?>
+	<form action="eval_imag.php" method="post">
+		<label for="cantFotos">¿Cuantas imágenes deseas ver?</label>
+		<select id="cantFotos" name="cantFotos">
+			<?php
+			cuantasImg();
+			for ($i = 2; $i <= cuantasImg(); $i++) {
+				echo "<option> $i </option>";
+			}
+			?>
+		</select><br>
+		<input type="submit" name="ver" value="VER IMAGENES">
+	</form>
+</body>
+
+</html>
